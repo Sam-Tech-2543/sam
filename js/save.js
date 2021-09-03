a008e9c1b6d30973728db3f04cba56ba9052f859
Sam-Tech-2543 <79782433+Sam-Tech-2543@users.noreply.github.com>
2021-09-03 11:44:00 +0530

    Update save.js

--- js/save.js
+++ js/save.js
@@ -1,19 +1,20 @@
 
-const firebaseConfig = {
-    apiKey: "AIzaSyDQB2apQDnZea8vymX-ZZANR-h4WYmiUuE",
-    authDomain: "resume1-c4a3b.firebaseapp.com",
-    projectId: "resume1-c4a3b",
-    storageBucket: "resume1-c4a3b.appspot.com",
-    messagingSenderId: "807151058550",
-    appId: "1:807151058550:web:3530c4c3901ef9fe7f6957",
-    measurementId: "G-YSGCRP299R"
-  };
-  
-  // Initialize Firebase
-const app = initializeApp(firebaseConfig);
-const analytics = getAnalytics(app);
+		const firebaseConfig = {
+            apiKey: "AIzaSyDQB2apQDnZea8vymX-ZZANR-h4WYmiUuE",
+            authDomain: "resume1-c4a3b.firebaseapp.com",
+            projectId: "resume1-c4a3b",
+            storageBucket: "resume1-c4a3b.appspot.com",
+            messagingSenderId: "807151058550",
+            appId: "1:807151058550:web:6b5db6a5357fd20e7f6957",
+            measurementId: "G-F6SX18F4BG"
+          };
+        
+          // Initialize Firebase
+          const app = initializeApp(firebaseConfig);
+          const analytics = getAnalytics(app);
 
 
+let contactInfo = firebase.database().ref("reviews");
 
 
 document.getElementById("contactform").addEventListener('submit',submitform);
@@ -26,5 +27,13 @@ function submitform(e){
     var msg = document.getElementById("msg").value;
 
     console.log(name,email,mobile,msg)
+    
+    let contactInfo = contactInfo.push();
+    newContactInfo.set({
+        name: name,
+        email: email,
+        mobile: mobile,
+        msg: msg,
+    })
 
 }
