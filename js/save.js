f8dda91e11163412cb98aebfdfe93d1997b1085c
Sam-Tech-2543 <79782433+Sam-Tech-2543@users.noreply.github.com>
2021-09-03 19:01:10 +0530

    Update save.js

--- js/save.js
+++ js/save.js
@@ -1,20 +1,4 @@
 
-		const firebaseConfig = {
-            apiKey: "AIzaSyDQB2apQDnZea8vymX-ZZANR-h4WYmiUuE",
-            authDomain: "resume1-c4a3b.firebaseapp.com",
-            projectId: "resume1-c4a3b",
-            storageBucket: "resume1-c4a3b.appspot.com",
-            messagingSenderId: "807151058550",
-            appId: "1:807151058550:web:6b5db6a5357fd20e7f6957",
-            measurementId: "G-F6SX18F4BG"
-          };
-        
-          // Initialize Firebase
-          const app = initializeApp(firebaseConfig);
-          const analytics = getAnalytics(app);
-
-
-let contactInfo = firebase.database().ref("reviews");
 
 
 document.getElementById("contactform").addEventListener('submit',submitform);
